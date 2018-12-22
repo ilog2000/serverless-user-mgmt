36de24f45ed4152688f159c7db9e433d88ddf4cb
Igor Loginov <igor.loginov@massive.co>
2018-12-22 21:59:20 +0100

    Add Babel runtime helpers to rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -5,30 +5,30 @@ import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
-	input: 'client/index.js',	
-	output: {
-		sourcemap: false,	
-		format: 'iife',
-		name: 'app',
-		file: 'public/bundle.js'
-	},
-	plugins: [
-		css({ output: 'public/bundle.css' }),
+  input: 'client/index.js',  
+  output: {
+    sourcemap: false,  
+    format: 'iife',
+    name: 'app',
+    file: 'public/bundle.js'
+  },
+  plugins: [
+    css({ output: 'public/bundle.css' }),
 
-		// If you have external dependencies installed from
-		// npm, you'll most likely need these plugins. In
-		// some cases you'll need additional configuration —
-		// consult the documentation for details:
-		// https://github.com/rollup/rollup-plugin-commonjs
-		resolve(),
-		commonjs(),
+    // If you have external dependencies installed from
+    // npm, you'll most likely need these plugins. In
+    // some cases you'll need additional configuration —
+    // consult the documentation for details:
+    // https://github.com/rollup/rollup-plugin-commonjs
+    resolve(),
+    commonjs(),
 
-		// If we're building for production (npm run build
-		// instead of npm run dev), transpile and minify
-		babel({
-			babelrc: false,
-			presets: [['@babel/preset-env', { modules: false }]],
-		}),
-		terser()
-	]
+    // If we're building for production (npm run build
+    // instead of npm run dev), transpile and minify
+    babel({
+      runtimeHelpers: true,
+      exclude: 'node_modules/**',
+    }),
+    terser()
+  ]
 };
